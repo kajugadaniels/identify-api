768aeaff16189c11949edd59539061270de58976
KAJUGA Daniels <kajugadaniels@gmail.com>
2026-03-19 16:06:58 +0200

    feat: add RegisterDto and LoginDto with class-validator rules and strong password policy

---
+++ src/auth/dto/register.dto.ts
@@ -0,0 +1,32 @@
+import {
+  IsEmail,
+  IsString,
+  MinLength,
+  MaxLength,
+  Matches,
+} from 'class-validator';
+
+export class RegisterDto {
+  @IsEmail({}, { message: 'Please provide a valid email address' })
+  email: string | undefined;
+
+  @IsString()
+  @MinLength(2, { message: 'First name must be at least 2 characters' })
+  @MaxLength(50)
+  firstName: string | undefined;
+
+  @IsString()
+  @MinLength(2, { message: 'Last name must be at least 2 characters' })
+  @MaxLength(50)
+  lastName: string | undefined;
+
+  @IsString()
+  @MinLength(8, { message: 'Password must be at least 8 characters' })
+  @MaxLength(100)
+  // Must contain: uppercase, lowercase, number, special character
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/, {
+    message:
+      'Password must contain uppercase, lowercase, number and special character',
+  })
+  password: string | undefined;
+}
